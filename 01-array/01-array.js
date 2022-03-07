0979cae2cb58de4336cf1e713410c548bd3d6b0a
Astuti Alawiya <asatutialawiya4131@gmail.com>
2022-03-07 20:32:06 +0700

    tugas-js-intermadiate

---
+++ 01-array/01-array.js
@@ -0,0 +1,54 @@
+//no.01
+const arr = [1, 2, 3];
+const arr2 = [8, 1, 5, 7];
+
+function reverse(array){
+    let new_array = [];
+    for (let i = 0; i< array.length; i++){
+        new_array[i] = array[array.length -i - 1];
+    } 
+    return new_array;
+}
+
+const newArr = reverse(arr);
+const newArr2 = reverse(arr2);
+
+console.log(arr, newArr);
+console.log(arr2, newArr2);
+
+//no.02
+const arr1 = [1, 3, 4, 1, 2, 8];
+const arr2 = [5, 6, 7, 8, 1, 3];
+
+function getAverage(arr) {
+  const total = arr.reduce((a, c) => a + c);
+  return total / arr.length;
+}
+
+console.log(getAverage(arr1))
+console.log(getAverage(arr2))
+
+//no.03
+const arr = [
+  [10],
+  [9, 7, 1],
+  [2, 8],
+];
+
+function searchInArray(arr, num) {
+  const newArr = arr.flat(1)
+ // console.log(newArr)
+  const findIndex = newArr.indexOf(num)
+ // console.log(findIndex)
+  if (findIndex == -1){
+    return null
+  } else {
+   return findIndex
+  }
+}
+
+
+console.log(searchInArray(arr, 3));
+console.log(searchInArray(arr, 2));
+console.log(searchInArray(arr, 4));
+console.log(searchInArray(arr, 8));
